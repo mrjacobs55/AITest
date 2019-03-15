b7e47be91d03edb7dd840a4c31a5dd9d6098bc57
Mitchell <mrjacobs55@gmail.com>
2019-03-15 12:39:06 -0400

    More testing options
    Signed-off-by: Mitchell <mrjacobs55@gmail.com>

--- ModelTesting.py
+++ ModelTesting.py
@@ -1,12 +1,12 @@
 from keras.models import load_model
 import numpy as np
 import csv
-import matplotlib.pyplot as plt
 
 model = load_model('testModel.h5')
 
-testData = np.empty([509444, 33])
-data = np.empty([509444, 33])
+
+testData = np.empty([23168, 33])
+data = np.empty([23168, 33])
 with open('WRegularSeasonDetailedResults.csv') as csvDataFile:
     csvReader = csv.reader(csvDataFile)
     i = 0
@@ -25,12 +25,63 @@ with open('WRegularSeasonDetailedResults.csv') as csvDataFile:
                     j += 1
         i += 1
 
-labels = np.empty([509444, 2])
+labels = np.empty([23168, 2])
 i = 0
 while i < len(labels):
     labels[i] = [0, 1]  # Low number for first = first team id wins
     i += 1
 
 
-print(model.evaluate(x=testData, y=labels, batch_size=1))
+print(model.evaluate(x=testData, y=labels, batch_size=64))
+
+print('WNCAA Tournament test')
+testData = np.empty([567, 33])
+with open('WNCAATourneyDetailedResults.csv') as csvDataFile:
+    csvReader = csv.reader(csvDataFile)
+    i = 0
+    for row in csvReader:
+        if i == 0:
+            print(row)
+        else:
+            testData[i-1] = row
+        i += 1
+
+labels = np.empty([567, 2])
+i = 0
+while i < len(labels):
+    labels[i] = [0, 1]  # Low number for first = first team id wins
+    i += 1
+
+
+print(model.evaluate(x=testData, y=labels, batch_size=64))
+
+print("Mens Season Test")
+testData = np.empty([41021, 33])
+data = np.empty([41021, 33])
+with open('MensRegularSeasonDetailedResults.csv') as csvDataFile:
+    csvReader = csv.reader(csvDataFile)
+    i = 0
+    k = 0;
+    j = 0;
+    for row in csvReader:
+        if i == 0:
+            print(row)
+        else:
+            if i%2 == 0:
+                data[k] = row
+                k += 1
+            else:
+                if i%2 == 1:
+                    testData[j] = row
+                    j += 1
+        i += 1
+
+labels = np.empty([41021, 2])
+i = 0
+while i < len(labels):
+    labels[i] = [0, 1]  # Low number for first = first team id wins
+    i += 1
+
+
+print(model.evaluate(x=testData, y=labels, batch_size=64))
 
--- TensorFlowTest.py
+++ TensorFlowTest.py
@@ -1,24 +1,34 @@
 from keras.models import Sequential
+from keras.models import load_model
 from keras.layers import Dense
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
-# For a single-input model with 2 classes (binary classification):
+newModel = False
+graph = False
+numSaveLoops = 2
+epochs = 250
+batchSize = 64
+saveModel = 'newModel.h5'
 
-model = Sequential()
-model.add(Dense(32, activation='relu', input_dim=33))
-model.add(Dense(30, activation='relu'))
-model.add(Dense(10, activation='relu'))
-model.add(Dense(2, activation='sigmoid'))
-model.compile(optimizer='adam',
+# For a single-input model with 2 classes (binary classification):
+if newModel:
+    model = Sequential()
+    model.add(Dense(32, activation='relu', input_dim=33))
+    model.add(Dense(30, activation='relu'))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dense(2, activation='sigmoid'))
+    model.compile(optimizer='adam',
               loss='binary_crossentropy',
               metrics=['accuracy'])
+else:
+    model = load_model(saveModel)
+#    model.load_weights('sampleWeightsLargeTest.h5')
 
-model.load_weights('sampleWeightsNew.h5')
 # Generate dummy data
-data = np.empty([509444, 33])
-testData = np.empty([509444, 33])
+data = np.empty([23168, 33])
+testData = np.empty([23168, 33])
 with open('WRegularSeasonDetailedResults.csv') as csvDataFile:
     csvReader = csv.reader(csvDataFile)
     i = 0
@@ -37,27 +47,25 @@ with open('WRegularSeasonDetailedResults.csv') as csvDataFile:
                     j += 1
         i += 1
 
-labels = np.empty([509444, 2])
+labels = np.empty([23168, 2])
 i = 0
 while i < len(labels):
     labels[i] = [0, 1]  # Low number for first = first team id wins
     i += 1
 
 # Train the model, iterating on the data in batches of 32 samples
-
-while True:
-    history = model.fit(data, labels,  epochs=250, batch_size=128)
+gen = 0
+while gen < numSaveLoops:
+    history = model.fit(data, labels,  epochs=epochs, batch_size=batchSize)
     model.save_weights('sampleWeightsLargeTest.h5')
-    model.save('testModel.h5')
+    model.save(saveModel)
+    gen += 1
 
+    plt.plot(history.history['acc'])
 
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.show()
-
-
-print(model.evaluate(x=testData, y=labels, batch_size=1))
+if graph:
+    plt.title('Model accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
